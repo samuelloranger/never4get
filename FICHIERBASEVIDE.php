d7867d38b272ddcfcb38a9f796f1492529e9c68d
Samuel <samuelloranger@gmail.com>
2018-11-15 08:58:02 -0500

    ajout commentaire

--- FICHIERBASEVIDE.php
+++ FICHIERBASEVIDE.php
@@ -34,7 +34,7 @@ $niveau = "./";
         crossorigin="anonymous"></script>
 
     <script>window.jQuery || document.write('<script src="node_modules/jquery/dist/jquery.min.js">\x3C/script>')</script>
-    <script src="js/validationsMandatB.js"></script>
+<!--    <script src="js/validationsMandatB.js"></script>-->
     <script>
         $('body').addClass('js');
         /**
