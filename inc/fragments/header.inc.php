fda06a1d95e498b7c757255377d4ab68345d28a9
Gabriel Chouinard Létourneau <chouinardletourneaug@gmail.com>
2018-11-15 09:33:23 -0500

    inc/fragments/header.inc: navigation prototype

---
+++ inc/fragments/header.inc.php
@@ -0,0 +1,10 @@
+<header>
+    <ul>
+        <li><a href="index.php">Accueil</a></li>
+        <li><a href="consulter-liste.php">Consulter une liste</a></li>
+        <li><a href="editer-liste.php">Éditer une liste</a></li>
+        <li><a href="ajouter-liste.php">Ajouter une liste</a></li>
+        <li><a href="editer-item.php">Éditer un item</a></li>
+        <li><a href="ajouter-item.php">Ajouter un item</a></li>
+    </ul>
+</header>
