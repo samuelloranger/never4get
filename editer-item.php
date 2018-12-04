98f7eb1cf72efb8eed74797b67bda8d7afb2947c
Samuel <samuelloranger@gmail.com>
2018-12-04 12:10:40 -0500

    update SL

--- editer-item.php
+++ editer-item.php
@@ -1,5 +1,5 @@
 <?php
-    //******************** Déclarations des variables ********************
+    //******************** DÉCLARATION DES VARIABLES ********************
     // Inclusion du fichier de configuration
      include($strNiveau . 'inc/scripts/config.inc.php');
 
@@ -9,20 +9,19 @@
     //Déclaration de la variable d'éxécution
     $strCodeOperation = "";
 
-    //Déclaration du code d'erreur
 
+   //******************** DATES ********************
     //Définit l'année de départ
     $dateAjd = new DateTime();
+    //Définit l'année du moment du chargement de la date.
     $anneeAjd = $dateAjd -> format('Y');
+    //Définit la date lors du chargement de la page parce qu'elle est effacée par la ligne du haut.
     $dateAjd = new DateTime();
 
-//    var_dump("test" . $dateAjd );
 
+   //******************** UTILITAIRES ********************
     //Tableau des mois pour affichage
     $arrMois = array("Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre");
-
-    //Code de date
-    $date_incomplete = false;
 
     //Récupération de query string
     if(isset($_GET["id_item"])){
@@ -34,10 +33,14 @@
         $strCodeOperation = "modifier";
     }
 
+
     //******************** Gestion des messages d'erreur ********************
+    //Va chercher le contenu du json dans le fichier objJSONMessages.json
     $strFichierJson = file_get_contents($strNiveau. "js/objJSONMessages.json");
+    //Décode et insére le contenu du json dans la variable
     $jsonMessagesErreurs = json_decode($strFichierJson);
 
+    //Array des champs qui sont en erreurs.
     $arrChampsErreurs = array();
 
     //Liste des message d'erreur à afficher
@@ -48,19 +51,25 @@
     //Code d'opération 00000 de base -> donc pas d'erreur
     $strCodeErreur = "00000";
 
-    //Message di
-
+    //Code de date incomplète qui change à true lorsque l'utilisateur
+    // entre seulement une partie de la date ou si le nom entré est vide
+    $date_incomplete = false;
+    $nomVide = false;
 
     //******************** Fonction utilitaires ********************
     //Fonction utilitaire pour l'affichage de la liste déroulante
+    //@param $valeurOption: Va chercher la valeur du option
+    //@param $nomSelection: Entre le nom du select
+    //return: Retourne rien OU selected="selected"
     function ecrireSelected($valeurOption, $nomSelection){
         $strSelection = "";
-        global $arrParticipant;
-        if($valeurOption == $arrParticipant[$nomSelection]){
+        global $arrInfosItem;
+        if($valeurOption == $arrInfosItem[$nomSelection]){
             $strSelection = 'selected="selected"';
         }
         return $strSelection;
     }
+
 
     //******************** Affichage des infos de l'item ********************
     $strRequeteInfosItem = "SELECT id_item, nom_item, DAY(echeance) AS jour, echeance, MONTH(echeance) AS mois , YEAR(echeance) AS annee,  HOUR(echeance) AS heure,  MINUTE(echeance) AS minute, t_item.id_liste, nom_liste
@@ -77,21 +86,19 @@
     //Éxécution de la requête
     $pdosResultatInfosItem -> execute();
 
+    //Insére les valeurs dans le $arrInfosItem pour utilisation ultérieure
     $arrInfosItem = $pdosResultatInfosItem -> fetch();
 
+    //Si la date n'est pas entrée, on remet les valeurs de l'heure et des minutes à -1
     if($arrInfosItem["echeance"] == null){
         $arrInfosItem["heure"] = -1;
         $arrInfosItem["minute"] = -1;
     }
 
+
     //******************** Ajouter/Modifier la date d'échéance ********************
     if ($strCodeOperation == "modifier") {
-        /**
-         * Pour info seulement :
-         * Lorsque le formulaire est envoyé, on fera des validations sur l'ensemble du formulaire
-         * et seulement si tout est correct, on ajoute l'occurrence dans la BD.
-         */
-
+        //On va chercher les valeurs dans la queryString
         $arrInfosItem["id_item"] = $_GET["id_item"];
         $arrInfosItem["id_liste"] = $_GET["id_liste"];
         $arrInfosItem["nom_item"] = $_GET["nom_item"];
@@ -120,6 +127,7 @@
                         else {
                             //Si l'heure et les minutes ne sont pas settés
                             if($arrInfosItem["heure"] == -1 AND $arrInfosItem["minute"] == -1){
+                                //On créer le format de la date dans une variable
                                 $dateSaisie = $arrInfosItem["annee"] . "-" . $arrInfosItem["mois"] . "-" . $arrInfosItem["jour"] . " 0:0";
                             }
                             else{
@@ -163,33 +171,45 @@
             }
         }
 
-
-        //Validation
+        //Validation du nom de l'item avec un regex
         if(!preg_match("/^[a-zA-Zà-ÿ0-9 \'\- #]{1,55}$/", $arrInfosItem["nom_item"])){
             $strCodeErreur = "-1";
             array_push($arrChampErreur, "nomItem");
         }
 
+        //Si le nom est vide
+        if($arrInfosItem["nom_item"] == ""){
+            $nomVide = true;
+        }
+
+        //Si le code d'erreur n'a pas été changé on passe dans la requête
         if($strCodeErreur == "00000"){
+            //Déclaration la requête
             $strRequeteUpdateInfosItem = "UPDATE t_item SET nom_item = :nom_item, echeance = :echeance WHERE id_item = :id_item";
 
+            //Préparation de la requête
             $pdosResultatModifierItem = $pdoConnexion -> prepare($strRequeteUpdateInfosItem);
 
+            //Attachement des valeurs de la queryString à la requête
             $pdosResultatModifierItem -> bindValue("id_item", $arrInfosItem["id_item"]);
             $pdosResultatModifierItem -> bindValue("nom_item", $arrInfosItem["nom_item"]);
             $pdosResultatModifierItem -> bindValue("echeance", $arrInfosItem["echeance"]);
 
+            //Éxécution de la requête
             $pdosResultatModifierItem -> execute();
         }
     }
 
     // *************************** Gestion des messages d'erreur **********************
+    //Si le code d'erreur a été changé = il y a au moins une erreur
     if($strCodeErreur != "00000"){
-        //Remplacer le message de confirmation par le message d'erreur
-//        $strMessage = $jsonMessagesErreurs -> {$champ};
+        //On boucle pour trouver toutes les erreurs
         for($intCtr = 0; $intCtr < count($arrChampsErreurs); $intCtr++){
+            //Définition du champ en erreur
             $champ = $arrChampsErreurs[$intCtr];
-            if($date_incomplete == true){
+
+            //Si c'est la date est mal entrée ou si le nom n'est pas entré
+            if($date_incomplete == true OR $nomVide == true){
                 $arrMessagesErreurs[$champ] = $jsonMessagesErreurs -> {$champ} -> {"erreurs"} -> {"vide"};
             }
             else{
@@ -197,10 +217,14 @@
             }
         }
     }
+    //S'il y n'y pas d'erreur
     else{
+        //Si le code d'opération est modifier,
         if($strCodeOperation == "modifier"){
+            //Redirection vers consulter-liste
             header("Location:" . $strNiveau . "consulter-liste.php?id_liste=" . $arrInfosItem["id_liste"] ."&btnOperation=modifier");
-//            var_dump("Location:" . $strNiveau . "consulter-liste.php?id_liste=" . $arrInfosItem["id_liste"] ."&strCodeOperation=modifier");
+            //Pour tests seulement
+            //var_dump("Location:" . $strNiveau . "consulter-liste.php?id_liste=" . $arrInfosItem["id_liste"] ."&strCodeOperation=modifier");
         }
     }
 
